3a9b25c10f83f67c0eb1eca3eecdb717b5c564b1
Jack Barber <jack@natureslaboratory.co.uk>
2021-03-22 13:34:44 +0000

    Update create_checkout_session.php

--- perch/templates/pages/create_checkout_session.php
+++ perch/templates/pages/create_checkout_session.php
@@ -1,27 +1,20 @@
 <?php
-/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
 
-use Slim\Http\Request;
-use Slim\Http\Response;
 use Stripe\Stripe;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
-
-$app->add(function ($request, $response, $next) {
-  \Stripe\Stripe::setApiKey('sk_test_51ITOTYGc8mzhKHdoEodxiWIjy0jZLIcEpP9R5M5Rk94x15IbEo6M0pz5HC7b4RsZmS6UebIXWhjeyxn3uI8xdAcb00qqqqHW6n');
-  return $next($request, $response);
-});
-
-$app->post('/create-checkout-session', function (Request $request, Response $response) {
-  $session = \Stripe\Checkout\Session::create([
-    'payment_method_types' => ['card'],
-    'line_items' => [[
+$stripe = new \Stripe\StripeClient(
+  'sk_test_51ITOTYGc8mzhKHdoEodxiWIjy0jZLIcEpP9R5M5Rk94x15IbEo6M0pz5HC7b4RsZmS6UebIXWhjeyxn3uI8xdAcb00qqqqHW6n'
+);
+$session = $stripe->checkout->sessions->create([
+  'success_url' => 'https://conference.iprg.info/success/',
+  'cancel_url' => 'https://conference.iprg.info',
+  'payment_method_types' => ['card'],
+  'line_items' => [[
       'price_data' => [
         'currency' => 'eur',
         'product_data' => [
@@ -30,13 +23,10 @@ $app->post('/create-checkout-session', function (Request $request, Response $res
         'unit_amount' => 2000,
       ],
       'quantity' => 1,
-    ]],
-    'mode' => 'payment',
-    'success_url' => 'https://conference.iprg.info/success/',
-    'cancel_url' => 'https://conference.iprg.info',
-  ]);
+  ]],
+  'mode' => 'payment',
+]);
 
-  return $response->withJson([ 'id' => $session->id ])->withStatus(200);
-});
+$id = $session->id;
 
-$app->run();
+echo $id;
