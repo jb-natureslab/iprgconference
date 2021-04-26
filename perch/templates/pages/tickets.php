b5e411ef4e0ac23fa5010bc8ba7b1a977d2657a3
Jack Barber <jack@natureslaboratory.co.uk>
2021-04-26 10:16:13 +0100

    Update tickets.php

--- perch/templates/pages/tickets.php
+++ perch/templates/pages/tickets.php
@@ -65,7 +65,7 @@
 			  console.log(clientSecret);
 			  // Call stripe.confirmCardPayment() with the client secret.
 			
-			var stripe = Stripe(\'pk_live_51ITOTYGc8mzhKHdom6HhbN4JxRmr4R3ndVK2HA8PU1dlD0rHn9Me4Sv9e31Kp0mCFhlxUP6o2a0rO8OiMEB3dz8Q00TdRSH1IZ\');
+			var stripe = Stripe(\'pk_test_51ITOTYGc8mzhKHdojEFogvwvSrtamy4fB7RakerixL3wxfxioLoR5GG3f3NaJf26YjJW6fM3QmzQMingLh2e58iH00mYJqu1UQ\');
 			var elements = stripe.elements();
 			
 			var elements = stripe.elements();
